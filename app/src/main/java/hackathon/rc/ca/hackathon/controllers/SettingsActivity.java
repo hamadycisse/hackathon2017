210b35181ddf38e4f93012c584b3a194ea1bb259
Boris Clain <borisclain1@gmail.com>
2017-03-25 20:52:54 -0400

    Résolution de "R" avec alt+enter

--- app/src/main/java/hackathon/rc/ca/hackathon/controllers/SettingsActivity.java
+++ app/src/main/java/hackathon/rc/ca/hackathon/controllers/SettingsActivity.java
@@ -17,6 +17,8 @@ import android.view.MenuItem;
 
 import java.util.List;
 
+import hackathon.rc.ca.hackathon.R;
+
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
  * handset devices, settings are presented as a single list. On tablets,
